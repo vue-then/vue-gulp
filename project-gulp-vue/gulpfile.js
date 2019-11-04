1344c420a5ea8e4b5432ce2fa0a3ba6cbc79895f
yyccQQu <643929860@qq.com>
2019-11-04 10:01:16 +0700

    change path

--- project-gulp-vue/gulpfile.js
+++ project-gulp-vue/gulpfile.js
@@ -6,17 +6,17 @@ var vueComponent    = require('gulp-vue-single-file-component');
 var runSequence     = require("gulp-run-sequence");
 var del             = require("del");
 
-gulp.task('scripts', () => gulp.src('./js/*.js')
+gulp.task('scripts', () => gulp.src('./src/*.js')
     .pipe(babel({ plugins: ['@babel/plugin-transform-modules-amd'] }))
-    .pipe(gulp.dest('./public/js'))
+    .pipe(gulp.dest('./public/src'))
     .pipe(browserSync.stream())
 );
  
-gulp.task('vue', () => gulp.src('./js/components/*.vue')
+gulp.task('vue', () => gulp.src('./src/components/*.vue')
     .pipe(vueComponent({ debug: true, loadCssMethod: 'loadCss' }))
     .pipe(babel({ plugins: ['@babel/plugin-transform-modules-amd'] }))
     .pipe(rename({ extname: '.js' }))
-    .pipe(gulp.dest('./public/js/components'))
+    .pipe(gulp.dest('./public/src/components'))
     .pipe(browserSync.stream())
 );
 
@@ -30,7 +30,7 @@ gulp.task('moveLib', function(){
 
 //删除包
 gulp.task("deljs", function () {
-    return del(["./public/js/*", "./public/js/**/*"], {
+    return del(["./public/src/*", "./public/src/**/*"], {
         dryRun: true
     }).then(paths => {
         del(paths);
@@ -46,15 +46,21 @@ gulp.task('watch', () => {
         }
     });
  
-    gulp.watch("./js/*.js", ["scripts"]);
-    gulp.watch("./js/components/*.vue", ["vue"]);
+    gulp.watch("./src/*.js", ["scripts"]);
+    gulp.watch("./src/components/*.vue", ["vue"]);
 });
  
 // // gulp.task('default', gulp.parallel('scripts', 'vue', 'watch'));
 
-gulp.task("build",function(){
-    
+gulp.task("vendor",function(){
+    // todo 外包依赖引入
 
+    
+})
+
+
+gulp.task("build",function(){
+    //todo 将 public 文件夹直接重新生成一个dist文件目录即可
 
 })
 
